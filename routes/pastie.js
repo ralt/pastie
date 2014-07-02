8143307eefe73208857de450449f1c739d10eff1
Florian Margaine <florian@margaine.com>
2014-07-02 22:47:00 +0200

    Adds error handling.

--- routes/pastie.js
+++ routes/pastie.js
@@ -11,14 +11,22 @@ exports.newGet = function(req, res) {
 exports.newPost = function(req, res) {
     pastie.add({ content: req.body.content }).then(function(pastie) {
         res.redirect('/' + pastie.uuid);
+    }).catch(function(err) {
+        console.error(err);
+        res.send(500);
     });
 };
 
 exports.getOne = function(req, res) {
     pastie.getOne(req.params.uuid).then(function(pastie) {
+        if (!pastie) throw new Error('No pastie found at /' + req.params.uuid);
+
         res.render('pastie', {
             title: 'Single pastie',
             code: pastie.content
         });
+    }).catch(function(err) {
+        console.error(err);
+        res.send(404);
     });
 };
